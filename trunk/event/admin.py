51c2434bac2dff16c20bc25377f465f582a45062
l0he1g <l0he1g@b9b4f1ee-9aa0-11dd-bd51-a78b85c98fae>
2009-04-05 00:00:49 +0000

    Gansha is transformed to mydreams

--- trunk/event/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from gansha.event.models import Event
-
-class EventAdmin(admin.ModelAdmin):
-    list_display=('id','title','description',
-                  'isprivacy','progress','num_se','start_date','end_date')
-    
-admin.site.register(Event,EventAdmin)
